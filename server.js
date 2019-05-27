cf452c8679f004b1f6749d87e960f89fcf960f49
Sandeep Gangwar <Sandeep.Gangwar@silicus.com>
2019-05-27 13:46:52 +0530

    Added missing files

--- server.js
+++ server.js
@@ -1,15 +1,67 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+app.set('view engine', 'ejs')
+
+
+var db
+var url = 'mongodb://localhost:27017';
+MongoClient.connect(url, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('dev') // whatever your database name is
 
 app.listen(3000,()=>{
 console.log('listening on 3000')
 });
 
+
+//CRUD Handlers
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use(express.static('public'))
+	
 app.get('/',(req,res)=>{
-	return res.send("<h1 style='color:red'>Recived Get Request</h1>");
+	db.collection('quotes').find().toArray((err,results)=>
+{
+if (err) return console.log(err)
+ res.render('index.ejs', {quotes: results})
 })
 
-app.get('/index',(req,res)=>{
-	return res.sendFile(__dirname+ '/index.html');
+	
 })
+
+app.get('/create',(req,res)=>{
+	return res.sendFile(__dirname+ '/create.html');
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.post('/quotes',(req,res)=>{
+   db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+})
+
+
 
