da4014c0418afb5ff97b00f9e74e7e5c0f104e5a
Sandeep Gangwar <Sandeep.Gangwar@silicus.com>
2019-05-27 12:01:34 +0530

    Get Request

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000,()=>{
+console.log('listening on 3000')
+});
+
+app.get('/',(req,res)=>{
+	return res.send("<h1 style='color:red'>Recived Get Request</h1>");
+})
+
+app.get('/index',(req,res)=>{
+	return res.sendFile(__dirname+ '/index.html');
+})
+
